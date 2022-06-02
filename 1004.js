e8cfc8d6f5e61592389e2ebebb3281f18612eaf0
Tom <yungsu2391@gmail.com>
2022-06-02 23:48:56 +0900

    6/2 백준 연습 문제> 어린왕자

--- 1004.js
+++ 1004.js
@@ -6,10 +6,39 @@
 const input = require('fs').readFileSync('./dummy/dummy_1004').toString().trim().split("\n");
 
 const casecnt = input.shift();
-const locations = input.shift();
-const planetcnt = input.shift();
 
-for(let i = 1; i <= planetcnt; i++) {
-  const [x, y, r] = input[i].split(" ").map(el => parseInt(el))
-  console.log(x,y,r)
+
+for(let i = 0; i<casecnt; i++){
+  let cnt = 0;
+  const locations = input.shift();
+  const planetcnt = input.shift();
+  for(let j = 1; j <= planetcnt; j++) {
+    const [ax, ay, bx, by] = locations.split(" ").map(e => parseInt(e));
+    const output = input.shift();
+    const [cx, cy, cr] = output.split(" ").map(el => parseInt(el));
+    cnt = cnt + r1(ax, ay, bx, by, cx, cy, cr);
+  }
+
+  console.log(cnt)
+}
+
+
+
+// 원이 좌표 1 기준으로는 안에 있고, 좌표 2기준으로는 밖에 있고,
+// 원이 좌표 2 기준으로는 안에 있고, 좌표 1기준으로는 밖에 있으면 됨
+
+function r1(ax, ay, bx, by, cx, cy, cr){
+  if(((cx-ax)**2+(cy-ay)**2)<cr**2){
+    if(((cx-bx)**2+(cy-by)**2)<cr**2){
+      return 0;
+    }else{
+      return 1;
+    }
+  }else{
+    if(((cx-bx)**2+(cy-by)**2)<cr**2){
+      return 1;
+    }else{
+      return 0;
+    }
+  }
 }
